b7eb0426c4be50aaa58b69968e82aba0172c75e4
olegpl1993 <olegpl1993@gmail.com>
2022-10-29 23:06:45 +0300

    copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs')
+const path = require('path')
 
+const pathToFolder = path.join(__dirname, 'files') // путь к папке files
+
+function fileInFolder() { // определяет список вложенных файлов
+  fs.readdir(pathToFolder, { withFileTypes: true }, (err, files) => { // делает массив обьектов вложенных в папку
+    files.forEach(file => { // перебор массива по обьектам
+      const pathToFile = path.join(pathToFolder, file.name) // путь к файлу
+      const newPathToFile = path.join(__dirname, 'files-copy', file.name) // новый путь к файлу
+      copyFile(pathToFile, newPathToFile)
+    })
+  })
+}
+
+function copyFile(pathToFile, newPathToFile) { // копирует файл из старой папки в новую
+  fs.copyFile(pathToFile, newPathToFile, err => {
+    if (err) throw err
+  })
+}
+
+fs.mkdir( // создает пустую папку
+  path.join(__dirname, 'files-copy'), // путь к новой папке
+  { recursive: true }, // если папка существует ошибки не будет
+  err => {
+    if (err) throw err
+    fileInFolder()
+  }
+)
