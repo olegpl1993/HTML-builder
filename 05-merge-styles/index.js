2656e551727a1a9ef5cb412a6984ada6dc348523
olegpl1993 <olegpl1993@gmail.com>
2022-10-30 21:17:25 +0200

    merge-styles v2

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,56 +4,20 @@ const path = require('path')
 const pathToStyles = path.join(__dirname, 'styles')
 const pathToDist = path.join(__dirname, 'project-dist')
 
-async function fileInFolder() { // опредляет css файлы в папке
-  return await fs.promises.readdir(pathToStyles, { withFileTypes: true })   // делает массив обьектов вложенных в папку
+async function fileInFolder() {
+  fs.writeFile(path.join(pathToDist, 'bundle.css'), '', err => { }) // создает bundle CSS
+  const arrCssFiles = []; // массив файлов css
+  const arrFiles = await fs.promises.readdir(pathToStyles, { withFileTypes: true }) // делает массив обьектов вложенных в папку
+  arrFiles.forEach(file => { if (file.isFile() && path.extname(file.name) === '.css') arrCssFiles.push(file.name) }) // выбирает css файлы
+  const arrCssText = []; // масив содержимого CSS файлов
+  for (let i in arrCssFiles) {
+    const pathToFile = path.join(pathToStyles, arrCssFiles[i]) // путь к текущему файлу
+    const data = await fs.promises.readFile(pathToFile, 'utf-8') // содержимое css файла
+    arrCssText.push(data) // заполняет массив информацией из css файлов
+  }
+  arrCssText.forEach((data) => { fs.appendFile(path.join(pathToDist, 'bundle.css'), data, err => { }) }) // заполняет bundle CSS
 }
 
-function cssFiles(arrFiles) {
-  const arrCssFiles = [];
-  arrFiles.forEach(file => { // перебор массива по обьектам
-    if (file.isFile() && path.extname(file.name) === '.css') { //  проверка что обьект является файлом и проверка расширение файла css
-      arrCssFiles.push(file.name)
-    }
-  })
-  return arrCssFiles
-}
-
-async function dataFromCSS(file) {
-  const pathToFile = path.join(pathToStyles, file)
-  const data = fs.promises.readFile(pathToFile, 'utf-8', (err, data) => { })
-  return data
-}
-
-async function pathCssFiles(arrCssFiles) {
-  const arrCssText = [];
-  const data0 = await dataFromCSS(arrCssFiles[0])
-  const data1 = await dataFromCSS(arrCssFiles[1])
-  const data2 = await dataFromCSS(arrCssFiles[2])
-  arrCssText.push(data0)
-  arrCssText.push(data1)
-  arrCssText.push(data2)
-  return arrCssText;
-}
-
-function add(arrCssText) { // добавляет ссобщение в файл
-  arrCssText.forEach((data) => {
-    fs.appendFile(
-      path.join(pathToDist, 'bundle.css'), data, err => { if (err) throw err }
-    )
-  })
-}
-
-async function createNewCss(arrCssText) {
-  fs.writeFile( // создает пустой файл
-    path.join(pathToDist, 'bundle.css'), '', err => { if (err) throw err }
-  )
-  add(arrCssText);
-}
-
-fileInFolder()
-  .then(arrFiles => cssFiles(arrFiles))
-  .then(arrCssFiles => pathCssFiles(arrCssFiles))
-  .then(arrCssText => createNewCss(arrCssText))
+fileInFolder();
 
 // node 05-merge-styles
-
