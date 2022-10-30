2229ce8b5ffe77e4452fc96e0a5085335422e534
olegpl1993 <olegpl1993@gmail.com>
2022-10-31 01:54:09 +0200

    files-in-folder improve function

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,15 +3,19 @@ const path = require('path')
 const { stdout } = process
 
 const pathToSecretFolder = path.join(__dirname, 'secret-folder') // путь к папке
-fs.readdir(pathToSecretFolder, { withFileTypes: true }, (err, files) => { // делает массив обьектов вложенных в папку
-  files.forEach(file => { // перебор массива по обьектам
-    if (file.isFile()) { // проверка что обьект является файлом
-      const pathToFile = path.join(__dirname, 'secret-folder', file.name) // путь к файлу
-      const [fileName, fileType] = file.name.split('.') // разделяем имена по точке
-      fs.stat(pathToFile, (err, stats) => { // получаем данные о файле
-        stdout.write(`${fileName} - ${fileType} - ${stats.size / 1024}kb\n`)
-      })
-    }
-  })
-})
 
+function filesInFolderInfo() { // выводит информацию о файлах в папке
+  fs.readdir(pathToSecretFolder, { withFileTypes: true }, (err, files) => { // массив обьектов вложенных в папку
+    files.forEach(file => { // перебор массива по обьектам
+      if (file.isFile()) { // проверка что обьект является файлом
+        const pathToFile = path.join(__dirname, 'secret-folder', file.name) // путь к файлу
+        const [fileName, fileType] = file.name.split('.') // разделяем имена по точке
+        fs.stat(pathToFile, (err, stats) => { // получаем данные о файле
+          stdout.write(`${fileName} - ${fileType} - ${stats.size / 1024}kb\n`) // выводим все полученные данные в консоль
+        })
+      }
+    })
+  })
+}
+
+filesInFolderInfo(pathToSecretFolder);
