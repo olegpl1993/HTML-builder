0a266cf68c6cfeeeea9346c2132f6784f4399a65
olegpl1993 <olegpl1993@gmail.com>
2022-10-31 18:08:06 +0200

    copy-directory clean new folder befor coping new files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,6 +6,14 @@ const pathToNewFolder = path.join(__dirname, 'files-copy') // путь для н
 
 function fileInFolder(pathToFolder, pathToNewFolder) { // создает копию папки со всеми вложеными файлами
   fs.mkdir(path.join(pathToNewFolder), { recursive: true }, err => { if (err) throw err }) // создает новую папку
+  // очищает папку если она осталась с предыдущего раза----------------
+  fs.readdir(pathToNewFolder, { withFileTypes: true }, (err, files) => { // массив обьектов вложенных в папку
+    files.forEach(file => { // перебор массива по обьектам
+      const newPathToFile = path.join(pathToNewFolder, file.name) // путь к файлу
+      fs.unlink(newPathToFile, err => { if (err) throw err }) // удаляет файл
+    })
+  })
+  // заполняет папку новыми файлами --------------------------------------
   fs.readdir(pathToFolder, { withFileTypes: true }, (err, files) => { // массив обьектов вложенных в папку
     files.forEach(file => { // перебор массива по обьектам
       const pathToFile = path.join(pathToFolder, file.name) // путь к файлу
